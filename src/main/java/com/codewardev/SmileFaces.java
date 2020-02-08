bd591081a297d5bdf907a1fac8a961b195064f44
Dale Wang <dalinwanglhtz@gmail.com>
2020-02-08 14:32:14 +1100

    first working cut

--- src/main/java/com/codewardev/SmileFaces.java
+++ src/main/java/com/codewardev/SmileFaces.java
@@ -1,19 +1,16 @@
 package com.codewardev;
 
-import java.util.Arrays;
-
 // https://www.codewars.com/kata/583203e6eb35d7980400002a/train/java
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class SmileFaces {
 
 	public static int countSmileys(List<String> arr) {
-		List<String> valid = Arrays.asList(":)", ":D", ";-D", ":~)");
+		Pattern p = Pattern.compile("^[:;]{1}[-~]?[)D]{1}$");
 		
-		arr.removeIf(x->(!valid.contains(x)));
-		
-		System.out.println("Size: "+arr.size());
+		arr.removeIf(x->(!p.matcher(x).matches()));
 		
 		return arr.size();
 	}
